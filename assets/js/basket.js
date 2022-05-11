1f7608fd3ce6aaf11ff8c164edc22f00086dfc12
mammadmammadli <memmed199@gmail.com>
2022-05-11 12:08:35 +0400

    Add counter for basket icon

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,9 +1,16 @@
+const itemcount = document.querySelector("#itemscount");
+
 (() => {
   const items = localStorage.getItem("items")
     ? JSON.parse(localStorage.getItem("items"))
     : [];
 
   if (items.length > 0) {
+    const countspan = document.createElement("span");
+    countspan.innerHTML = items.length;
+
+    itemcount.appendChild(countspan);
+
     items.forEach((item) => {
       const icon = document.querySelector(`#${item.id} > i`);
 
@@ -15,6 +22,11 @@
 
 function toggleItemBasket(id) {
   const icon = document.querySelector(`#${id} > i`);
+  const span =
+    document.querySelector("#itemscount > span") ??
+    document.createElement("span");
+
+  console.log(span);
 
   let items = localStorage.getItem("items")
     ? JSON.parse(localStorage.getItem("items"))
@@ -43,7 +55,13 @@ function toggleItemBasket(id) {
 
     icon.classList.add("fa-solid");
     icon.classList.remove("fa-regular");
+    itemcount.appendChild(span);
   }
 
+  if (items.length === 0) {
+    itemcount.removeChild(span);
+  } else {
+    span.innerHTML = items.length;
+  }
   localStorage.setItem("items", JSON.stringify(items));
 }
